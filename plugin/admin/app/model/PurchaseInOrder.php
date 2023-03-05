ecafa7aee6a1ea8c5535f82b8147036e560ae7a0
ffsong <cq_tans@163.com>
2023-03-05 16:08:28 +0800

    添加软删除

--- plugin/admin/app/model/PurchaseInOrder.php
+++ plugin/admin/app/model/PurchaseInOrder.php
@@ -2,6 +2,7 @@
 
 namespace plugin\admin\app\model;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use plugin\admin\app\model\Base;
 
 /**
@@ -19,6 +20,7 @@ use plugin\admin\app\model\Base;
  */
 class PurchaseInOrder extends Base
 {
+    use SoftDeletes;
     /**
      * The table associated with the model.
      *
